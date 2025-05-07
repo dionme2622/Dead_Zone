2e42f35e34b4d4669a6af785989b44909da5be6d
rladuddbs <duddbs442@naver.com>
2025-05-07 20:25:06 +0900

    좀비한테 플레이어스테이트 추가

--- Engine/BattleScene.cpp
+++ Engine/BattleScene.cpp
@@ -266,6 +266,7 @@ void BattleScene::LoadScene()
 			}
 				gameObjects[23]->GetTransform()->SetLocalPosition(Vec3(i * 5.0f, 80.0f, 0.0f));
 				gameObjects[23]->AddComponent(make_shared<CharacterController>(gameObjects[23], 0.5, 3.0, 0.3f));
+				gameObjects[23]->AddComponent(make_shared<PlayerStats>());
 				gameObjects[23]->GetCharacterController()->OnEnable();
 		}
 	}
